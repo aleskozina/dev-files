379b85ffc5978673ac2ea05b8bfb98e2c9219df3
Ales Kozina <39375089+aleskozina@users.noreply.github.com>
2018-11-18 20:55:06 +0100

    Update install-new.sh

--- install-new.sh
+++ install-new.sh
@@ -7,49 +7,52 @@ function install {
 	printf "${GREEN}install.sh => ${NC}${BLUE}$1${NC}${GREEN} installing${NC}\n";
 	return 0; 
 }
-
-function needed {
-	if ! command -v $1 >/dev/null 2>&1 || [ ! -z $3 ]
-	then
-		install "$2"
-		return 0;
-	else
-		printf "${GREEN}install.sh => ${NC}${BLUE}$2${NC}${GREEN} already installed${NC}\n";
-		return 1;
-	fi;
-	return 1;adsf+"*"
-}
+function installed {
+	printf "${GREEN}install.sh => ${NC}${BLUE}$1${NC}${GREEN} already installed${NC}\n";
+	return 0;
+} 
 
 function success {
 	printf "${GREEN}install.sh => ${NC}${BLUE}$1${NC}${GREEN} installed${NC}\n";
 }
 
 ## brew
-needed brew Homebrew \
-	&& ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null \
-	&& success Homebrew
+command -v brew >/dev/null 2>&1 \
+	&& installed brew \
+	|| (install brew && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null \
+	&& success brew)
 
 ## code via brew
-needed code "Visual Studio Code" \
-	&& brew cask install visual-studio-code \
-	&& success "Visual Studio Code"
+brew cask ls --versions visual-studio-code >/dev/null \
+	&& installed visual-studio-code \
+	|| (install visual-studio-code && brew cask install visual-studio-code && success visual-studio-code)
+	
 
 # zsh
-needed zsh Zsh force \
-	&& brew install zsh \
-	&& success Zsh
+brew ls --versions zsh >/dev/null \
+	&& installed zsh \
+	|| (install zsh && brew install zsh && success zsh)
 
 # ohmyzsh
-install "oh-my-zsh" \
-	&& sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" \
-	&& success "oh-my-zsh"
+[ -d ~/.oh-my-zsh/ ] \
+	|| (install oh-my-zsh && h -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && success "oh-my-zsh") \
+	&& installed oh-my-zsh
 
 # zsh-syntax-highlighting
-install "zsh-syntax-highlighting" \
-	&& brew install zsh-syntax-highlighting \
-	&& success "zsh-syntax-highlighting"
+brew ls --versions zsh-syntax-highlighting >/dev/null \
+	&& installed zsh-syntax-highlighting \
+	|| (install zsh-syntax-highlighting && brew install zsh-syntax-highlighting && success "zsh-syntax-highlighting")
 
-needed nvm "Node Version Manager" \
-	&& (curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash) \
-	&& success "Node Version Manager"
-	
+[ -d ~/.nvm/ ] \
+	&& installed nvm \
+	|| (install nvm && (curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash) && success nvm)
+
+# zsh
+brew ls --versions git >/dev/null \
+	&& installed git \
+	|| (install git && brew install git && success git)
+
+# hyper.js
+brew cask ls --versions hyper >/dev/null \
+	&& installed hyper \
+	|| (install hyper && brew cask install hyper && success hyper)
